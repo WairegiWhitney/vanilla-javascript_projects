366f44ad2ba683201248b8384dd57248a2d8231d
Wairegi Whitney <wairegiwhitney11@gmail.com>
2024-10-11 12:51:42 +0300

    counter

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -1,8 +1,13 @@
-const colors=["blue","red","green","rgba(133,122,200)","#f15025"];
-const btn=document.querySelector(".btn");
+const colors = ["green","red","rgba(133,122,200)","#f15025"];
+const btn= document.getElementById("btn");
 const color=document.querySelector(".color");
-btn.addEventListener("click",()=>{
-    let random=Math.floor(Math.random()*colors.length);
-    document.body.style.backgroundColor=colors[random];
-    color.textContent=colors[random]
+
+btn.addEventListener("click", function(){
+const randomNumber=getRandomNumber();
+document.body.style.backgroundColor=colors[randomNumber];
+color.textContent=colors[randomNumber];
 });
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
